7bdf836bf2e719a28d862e88c0b9a59a189a6366
omer5 <omer5144@gmail.com>
2021-05-28 23:01:31 +0300

    start create date picker

--- app/src/main/java/com/example/smartutor/StudentSignUp.java
+++ app/src/main/java/com/example/smartutor/StudentSignUp.java
@@ -1,7 +1,10 @@
 package com.example.smartutor;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
@@ -9,6 +12,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
 
 public class StudentSignUp extends Fragment {
 
@@ -22,5 +28,25 @@ public class StudentSignUp extends Fragment {
         View view = inflater.inflate(R.layout.fragment_student_sign_up, container, false);
 
         return view;
+    }
+
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            // Do something with the date chosen by the user
+        }
     }
 }
