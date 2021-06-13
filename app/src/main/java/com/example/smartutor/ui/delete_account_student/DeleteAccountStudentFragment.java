5266f8a5474c3956bd988792c6cc65ade9f1f49a
omer5 <omer5144@gmail.com>
2021-06-13 19:01:58 +0300

    tutor pages

--- app/src/main/java/com/example/smartutor/ui/delete_account_student/DeleteAccountStudentFragment.java
+++ app/src/main/java/com/example/smartutor/ui/delete_account_student/DeleteAccountStudentFragment.java
@@ -16,10 +16,8 @@ public class DeleteAccountStudentFragment extends Fragment {
 
     private DeleteAccountStudentViewModel deleteAccountStudentViewModel;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        deleteAccountStudentViewModel =
-                new ViewModelProvider(this).get(DeleteAccountStudentViewModel.class);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        deleteAccountStudentViewModel = new ViewModelProvider(this).get(DeleteAccountStudentViewModel.class);
         View root = inflater.inflate(R.layout.fragment_delete_account_student, container, false);
         return root;
     }
--- app/src/main/java/com/example/smartutor/ui/edit_details_tutor/EditDetailsTutorFragment.java
+++ app/src/main/java/com/example/smartutor/ui/edit_details_tutor/EditDetailsTutorFragment.java
@@ -17,14 +17,23 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
 
+import com.example.smartutor.MultiSpinner;
 import com.example.smartutor.R;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
+import java.util.Arrays;
 import java.util.Date;
 
 public class EditDetailsTutorFragment extends Fragment {
 
     private EditDetailsTutorViewModel editDetailsTutorViewModel;
+    //views
+    private Spinner gender;
+    private EditText password;
+    private EditText confirm;
+    private MultiSpinner profesions;
+    private Button chooseDate;
+    private EditText date;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // view model
@@ -32,6 +41,32 @@ public class EditDetailsTutorFragment extends Fragment {
 
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_edit_details_tutor, container, false);
+
+        // views setup
+        gender = root.findViewById(R.id.editDetailsTutor_gender_spn);
+        password = root.findViewById(R.id.editDetailsTutor_password_et);
+        confirm = root.findViewById(R.id.editDetailsTutor_confirmPassword_et);
+        profesions = root.findViewById(R.id.editDetailsTutor_professions_spn);
+        chooseDate = root.findViewById(R.id.editDetailsTutor_birthdayDate_btn);
+        date = root.findViewById(R.id.editDetailsTutor_birthdayDate_et);
+
+        ArrayAdapter<CharSequence> genderAdapter = ArrayAdapter.createFromResource(this.getActivity(), R.array.gender, R.layout.spinner_item);
+        gender.setAdapter(genderAdapter);
+
+        profesions.setItems(Arrays.asList(getResources().getStringArray(R.array.subject)), "Choose professions.", (MultiSpinner.MultiSpinnerListener) selected -> { });
+
+        // events setup
+        chooseDate.setOnClickListener(v -> {
+            DatePicker picker = new DatePicker(getContext());
+            picker.setCalendarViewShown(false);
+            picker.setMaxDate(new Date().getTime());
+            MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(getContext(), R.style.AlertDialogStyle)
+                    .setView(picker)
+                    .setPositiveButton("OK", (dialog, which) -> dialog.cancel())
+                    .setOnCancelListener(dialog -> date.setText(picker.getDayOfMonth()+"/"+ (picker.getMonth() + 1)+"/"+picker.getYear()));
+            builder.show();
+        });
+
 
         return root;
     }
